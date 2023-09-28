e5c3d34a58aaffcbae9c381151f768da45ba9a36
Roman <rchernigovskix@rambler.ru>
2023-09-28 21:11:31 +0300

    добавил чтение индульгенций

---
+++ TournamentBuilderLib/Handlers/GetIndulgetnciaHandler.cs
@@ -0,0 +1,22 @@
+﻿using ExcelLib;
+using TournamentBuilderLib.Models;
+
+namespace TournamentBuilderLib.Handlers;
+
+public interface IGetIndulgetnciaHandler
+{
+    public int Execute(IParticipant participant);
+}
+
+public class GetIndulgetnciaHandler : IGetIndulgetnciaHandler
+{
+    private const string SHEET_NAME = "Список участников";
+    private string SHEET_ID = GoogleSheet.Default.SHEET_ID;
+
+    public int Execute(IParticipant participant)
+    {
+        var cellAdress = $"{SHEET_NAME}!D{participant.Id}";
+        var values = ExcelReader.Read(SHEET_ID, cellAdress);
+        return Convert.ToInt32(values.FirstOrDefault()?[0]);
+    }
+}
